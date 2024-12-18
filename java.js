9f45e60d1291998c5cb46712347bd33c994f06b7
omarmeroo32 <omarmeroo921@gmail.com>
2024-12-18 08:48:40 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,10 @@
+let photoar = document.getElementById("photoar");
+let container = document.querySelector('.container');
+
+function photos(source) {
+  photoar.src = source;
+}
+function color (colors){
+    container.style.background = colors
+}
+
